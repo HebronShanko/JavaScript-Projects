2eee169f3512242cde8e9a3ed636fbbfcebcc0a8
HebronShanko <hebronshanko@gmail.com>
2021-07-08 18:28:22 -0700

    assignment 2

---
+++ Project2_functions/js/main.js
@@ -0,0 +1,5 @@
+function myfirstFunction () { //naming function
+  let str = "this is supposed to be red";
+  str += " and this is part of the sentetnce"; //+= operator
+  document.getElementById("red_text") .innerHTML = str;
+}
