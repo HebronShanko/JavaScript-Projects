289a4f1d4e3bc19f01607f59f9d9cf40229b5b84
HebronShanko <hebronshanko@gmail.com>
2021-07-08 17:08:33 -0700

    basic javascript assignment

---
+++ Project1_expressions_alert/JS./main.js
@@ -0,0 +1,9 @@
+
+var alrt = ('this is an alert'); //alert method
+  window.alert(alrt);
+
+  document.write(alrt);   //document.write method
+ 
+var strng = ('words') , intgrs = ('numbers wether with floats or not') , boolean = ('true or not')
+var intgrs = intgrs.fontcolor("blue");
+  document.write(intgrs);   //color method
