a22d11eb4c483a9bbe4fd0e44c5288f275ed06bb
HebronShanko <hebronshanko@gmail.com>
2021-07-09 18:19:40 -0700

    assignment operations

---
+++ Project3_math_operators/JS/main.js
@@ -0,0 +1,40 @@
+function subtraction_function() {
+  var subtraction = 7-3;
+  document.getElementById("Math-").innerHTML = "7-3 = " + subtraction;
+} 
+
+function multiplication_function() {
+  var multiplication = 9*9;
+  document.getElementById("Math*").innerHTML = "9*9 = " + multiplication;
+}
+
+function addition_function() {
+  var addition = 5+5;
+  document.getElementById("Math+").innerHTML = " 5+5 = " + addition;
+}
+
+function division_function() {
+  var division = 90/3;
+  document.getElementById("Math/").innerHTML = " 90/30 = " + division
+}
+
+//operations above
+
+let x = 5;
+x++;
+let h = x;
+document.write(h);
+
+let y = 10;
+y--;
+let z = y;
+document.write(z);
+
+//increment and decrement
+
+
+window.alert(Math.random() *100);
+
+//random number
+
+
