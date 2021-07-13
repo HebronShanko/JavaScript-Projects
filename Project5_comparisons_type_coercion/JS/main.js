de674cb95e6eab0cddeca29e107b7ffe306ffe9c
HebronShanko <hebronshanko@gmail.com>
2021-07-13 09:14:31 -0700

    Assigment

---
+++ Project5_comparisons_type_coercion/JS/main.js
@@ -0,0 +1,43 @@
+document.write(typeof "word")
+
+
+document.write("10"+10);
+//double equal sign
+
+document.write(10 == 10);
+
+document.write(3 == 11)
+;
+
+//tripple equal sign
+
+x=10;
+y=10;
+document.write(x===y);
+
+a=50;
+b = "50";
+document.write(a===b);
+
+c = "Bob";
+d = "Bob";
+document.write(c===b);
+
+//logical operation
+
+document.write(5>2 && 10>4);
+
+document.write(5>10 && 10>4);
+
+document.write(5>10 || 10>4);
+
+document.write(5>10 || 10>4);
+
+document.write(5>10 || 10>20);
+
+//not operation
+
+function not_Function(){
+  document.getElementById("Not").innerHTML = !(20 > 10);
+}
+
