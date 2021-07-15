aa55aa97016f7bb421eda4869d4d845e8fdc3005
HebronShanko <hebronshanko@gmail.com>
2021-07-15 13:51:23 -0700

    assignments

--- Project10_loops_arrays/JS/main.js
+++ Project10_loops_arrays/JS/main.js
@@ -1,3 +1,5 @@
+//using let and while loop
+
 function Call_Loop() {
   let digit = "";
   let x = 1;
@@ -6,4 +8,27 @@ function Call_Loop() {
     x++;
   }
   document.getElementById("Loop").innerHTML = digit;
+}
+
+//for loop
+
+var Instruments = ["drums", "guitar", "piano", "bass", "flute", "trumpet", "violin"];
+var Content = "";
+var Y;
+function for_loop() {
+  for (Y = 0; Y < Instruments.lenght; Y++) {
+    Content += Instruments[Y] + "<br>";
+  }
+  document.getElementById("List_of_Instruments").innerHTML = Content;
+}
+
+//array
+
+function cat_pics(){
+  let Cat_picture = [];
+  Cat_picture[0] = " sleeping";
+  Cat_picture[1] = " playing";
+  Cat_picture[2] = " eating";
+  Cat_picture[3] = " purring";
+  document.getElementById("Cat").innerHTML = "in this picture, the cat is" + Cat_picture[2] + ".";
 }
